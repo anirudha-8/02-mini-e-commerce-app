43a272d8100fa37890849fbaf3c595313d8458f2
Anirudha Bele <beleanirudha8@gmail.com>
2025-06-11 23:04:42 +0530

    feat(backend): complete backend logic

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,34 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import cors from "cors";
 
-app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+// to use environment variables
+dotenv.config();
+
+const app = express();
+
+const port = 8080;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
+// root route
+app.get("/", (req, res) => res.send("hello"));
+
+app.use("/api/product", productRoutes);
+
+const startServer = async () => {
+	try {
+		await connectDB();
+		app.listen(port, () => {
+			console.log(`Server started at: http://localhost:${port}`);
+		});
+	} catch (error) {
+		console.error(`Failed to start the server: ${error.message}`);
+	}
+};
+
+startServer();
